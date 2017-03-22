a3b5d09c919585bfbd0f73b2114d97296fc28e4b
Steve Smith <stevesmith2609@gmail.com>
2017-03-22 22:13:29 +0000

    Update crate storer methods to accept crates with coords

--- app/services/crate_storer.rb
+++ app/services/crate_storer.rb
@@ -1,14 +1,12 @@
 class CrateStorer
-  def initialize(shelving_unit, x, y, crate)
+  def initialize(shelving_unit, crate)
     @shelving_unit = shelving_unit
-    @x = x
-    @y = y
     @crate = crate
   end
 
   def call
-    coords = crate_position_coordinates(@crate, @x, @y)
-    if crate_fits_in_shelving_unit?(coords, @shelving_unit)
+    coords = crate_position_coordinates(@crate)
+    if crate_fits_in_shelving_unit?(coords)
       coords.each { |coord| @shelving_unit.representation[coord[:y]][coord[:x]] = @crate }
     else
       return false
@@ -17,17 +15,17 @@ class CrateStorer
 
   private
 
-  def crate_position_coordinates(crate, x, y)
+  def crate_position_coordinates(crate)
     arr = []
     crate.height.times do |row|
       crate.width.times do |column|
-        arr << { x: x + column, y: y + row }
+        arr << { x: crate.x + column, y: crate.y + row }
       end
     end
     arr
   end
 
-  def crate_fits_in_shelving_unit? coords, shelving_unit
+  def crate_fits_in_shelving_unit?(coords)
     coords.all? { |coord| @shelving_unit.in_position(coord[:x], coord[:y]) == nil }
   end
 end
--- test/services/test_crate_storer.rb
+++ test/services/test_crate_storer.rb
@@ -6,38 +6,42 @@ require './app/services/crate_storer'
 class TestCrateStorer < Minitest::Test
   def setup
     @shelving_unit = ShelvingUnit.new(3, 3)
-    @crate = Crate.new(2, 1, "P")
+    @crate = Crate.new(1, 0, 2, 1, "P")
   end
 
   def test_that_valid_crate_is_stored_correctly
-    CrateStorer.new(@shelving_unit, 1, 0, @crate).call
+    CrateStorer.new(@shelving_unit, @crate).call
     assert_equal [[nil, @crate, @crate],[nil, nil, nil],[nil, nil, nil]], @shelving_unit.representation
   end
 
   def test_that_crate_cannot_be_stored_outside_x_bounds_of_shelving_unit
-    storage = CrateStorer.new(@shelving_unit, 3, 1, @crate).call
+    crate = Crate.new(3, 1, 2, 1, "P")
+    storage = CrateStorer.new(@shelving_unit, crate).call
     assert_equal false, storage
   end
 
   def test_that_crate_cannot_be_stored_outside_y_bounds_of_shelving_unit
-    storage = CrateStorer.new(@shelving_unit, 3, 4, @crate).call
+    crate = Crate.new(1, 4, 2, 1, "P")
+    storage = CrateStorer.new(@shelving_unit, crate).call
     assert_equal false, storage
   end
 
-  def test_that_crate_cannot_be_stored_over_bounds_of_shelving_unit
-    storage = CrateStorer.new(@shelving_unit, 3, 2, @crate).call
+  def test_that_crate_cannot_be_stored_overlapping_bounds_of_shelving_unit
+    crate = Crate.new(2, 2, 2, 2, "P")
+    storage = CrateStorer.new(@shelving_unit, crate).call
     assert_equal false, storage
   end
 
   def test_that_crate_cannot_be_stored_in_another_crates_spot
-    CrateStorer.new(@shelving_unit, 1, 0, @crate).call
-    storage = CrateStorer.new(@shelving_unit, 1, 0, @crate).call
+    CrateStorer.new(@shelving_unit, @crate).call
+    storage = CrateStorer.new(@shelving_unit, @crate).call
     assert_equal false, storage
   end
 
   def test_that_crate_cannot_be_stored_over_bounds_of_another_crate
-    CrateStorer.new(@shelving_unit, 1, 0, @crate).call
-    storage = CrateStorer.new(@shelving_unit, 0, 0, @crate).call
+    CrateStorer.new(@shelving_unit, @crate).call
+    crate = Crate.new(2, 0, 2, 1, "P")
+    storage = CrateStorer.new(@shelving_unit, @crate).call
     assert_equal false, storage
   end
 end
